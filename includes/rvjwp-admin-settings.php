f9ec2a78db670e00c407cea9dc0d73087de892d9
ilGhera <andrea@ilghera.com>
2017-05-04 23:26:53 +0200

    Update to release 1.1

--- includes/rvjwp-admin-settings.php
+++ includes/rvjwp-admin-settings.php
@@ -42,15 +42,16 @@ function rvjwp_add_menu() {
 function rvjwp_options() {
 	
 	echo '<div class="wrap">'; 
-	echo '<h2>' . __('Related Videos for JW Player', 'rvjwp-lang') . '</h2>';
+	echo '<h1>' . __('Related Videos for JW Player', 'rvjwp-lang') . '</h1>';
 	echo '<p>' . __('If you\'re using <strong>JW Player</strong> on your site, you probably know <strong><a href="http://support.JW Player.com/customer/portal/articles/1409745-display-related-videos" target="_blank">Related Videos</a></strong>, a free plugin that allows you to show more contents to the users in a beautifull and simple layout.', 'rvjwp-lang') . '<br>';
 	echo __('<strong>Related Videos for JW Player</strong> will creates the correct xml for each category, so you\'ll be able to show related contents dynamically.', 'rvjwp-lang') . '</p>';
 	echo '<p>' . __('Select how to get the video thumbnail from your posts.', 'rvjwp-lang') . '</p>';
 
 	include(plugin_dir_path( __FILE__ ) . '/rvjwp-form.php');
 
-	echo '<p>' . __('In your JW Player code, you\'ll have something like that:', 'rvjwp-lang');
-	
+	echo '<p>' . __('In your JW Player code, you\'ll have something like that.', 'rvjwp-lang') . '<br>';
+	echo __('Please, check out your <a href="' . home_url() . '/wp-admin/options-permalink.php">permalink structure</a> for a correct category link.', 'rvjwp-lang');	
+
 	echo '<pre>\'related\': {
    \'file\': \'<span>http://YOUR-SITE/archives/category/THE-CATEGORY-SLUG/?feed=related-feed</span>\',
    \'heading\': \'More videos!\',
@@ -61,7 +62,6 @@ function rvjwp_options() {
 	echo '<p>' . __('And that\'s the result:', 'rvjwp-lang') . '</p>';
 
 	echo '<img class="rvjwp-image" src="' . plugins_url('images/rvjwp-image.jpg', 'related-videos-for-jw-player/images')  . '" />';
-
 	echo '</div>';
 
 }
